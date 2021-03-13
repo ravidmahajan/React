af9b47d41acb19a789aad2ff9bdf2e2d1b22c6c7
Ravi Mahajan <ravi.d.mahajan@gmail.com>
2021-03-14 00:13:41 +0530

    State class based and function based components + React Hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,111 @@
-import { Component } from 'react';
+
+// FUNCTION BASED COMPONENT
+// ==============================================================================
+// React Hooks always start with 'use'
+
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
 
-    state = {
+const App = props => {
+
+   const [personsState, setPersonsState] =  useState({
         persons : [
             { name: 'Ravi', surname: 'Mahajan' },
             { name: 'Raj', surname: 'Mahajan' }
         ]
-    }
+        //,otherState: 'some other value'
+    });
 
-    render(){
-        return ( <div className = "App">
-        <h1> Hi I am React app </h1>  
-        <Person name = { this.state.persons[0].name }
-        surname = { this.state.persons[0].surname }>Hey hiii! </Person>
-        <Person name = { this.state.persons[1].name }
-        surname = { this.state.persons[1].surname }/> 
-        </div>
-    );
-    }
+    //useState({otherState: 'some other value'});
+
     
+
+    const [otherState, setOtherState] = useState('some other value');
+
+    console.log(personsState, otherState);
+
+    //useState returns array with exactly 2 elements  
+    //1. - current state and then updated state 
+    //2. - function which will update the state, such that react is aware of it and rerender the component
+    // 2nd parameter doesnot merge whatever we pass with old state instead it replaces old state
+    //we have to manually make sure we include old  state data
+    // [1] elegant way is do not maually ,erge but use useState multiple times
+    //whereas in class based components , we have setState property which automatically merges old state 
+    //and not discard any state, here we can have multiple useState calls.
+
+    const switchNameHandler = () => {
+    //console.log('button was clicked');
+    // dont use this.state.persons[0].name = 'Maximilian';
+
+    setPersonsState({
+        persons : [
+            { name: 'Rahul', surname: 'Mahajan' },
+            { name: 'Raj', surname: 'Mahajan' }
+        ]
+        // [1], otherState : personsState.otherState
+    })
 }
 
+        return ( <div className = "App">
+        <h1> Hi I am React app </h1> 
+        <button onClick={switchNameHandler}>Click here</button>
+        <Person name = { personsState.persons[0].name }
+        surname = { personsState.persons[0].surname }>Hey hiii! </Person>
+        <Person name = { personsState.persons[1].name }
+        surname = { personsState.persons[1].surname }/> 
+        </div>
+    );
+}
 export default App;
+
+
+
+
+
+// CLASS BASED COMPONENT
+// =================================================================================
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+
+
+
+
+
+// class App extends Component {
+
+//     state = {
+//         persons : [
+//             { name: 'Ravi', surname: 'Mahajan' },
+//             { name: 'Raj', surname: 'Mahajan' }
+//         ],
+//          otherState: 'some other value'
+//     }
+
+//     switchNameHandler = () => {
+//     //console.log('button was clicked');
+//     // dont use this.state.persons[0].name = 'Maximilian';
+
+//     this.setState({
+//         persons : [
+//             { name: 'Rahul', surname: 'Mahajan' },
+//             { name: 'Raj', surname: 'Mahajan' }
+//         ]
+//     })
+// }
+
+//     render() {
+//         return ( <div className = "App">
+//         <h1> Hi I am React app </h1> 
+//         <button onClick={this.switchNameHandler}>Click here</button>
+//         <Person name = { this.state.persons[0].name }
+//         surname = { this.state.persons[0].surname }>Hey hiii! </Person>
+//         <Person name = { this.state.persons[1].name }
+//         surname = { this.state.persons[1].surname }/> 
+//         </div>
+//     );
+//     }   
+// }
+// export default App;
