d3b602735243c935eeb1cac145f21dc83cf36300
Ravi Mahajan <ravi.d.mahajan@gmail.com>
2021-03-14 00:34:00 +0530

    event handlers passed as reference

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => { 
     return (
     <div><p> { props.name } </p>
-    <p> { props.surname } </p>
+    <p onClick={props.click}> { props.surname } </p>
     <p> { props.children } </p>
     </div>);
 
